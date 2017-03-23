a3f543f842b1d088db6a68148bd10260cb327be7
jjmois <heyjjmois@gmail.com>
2017-03-22 23:47:15 -0700

    Update dashboard-items.js

--- about/talkturkey/dashboard-items.js
+++ about/talkturkey/dashboard-items.js
@@ -1,32 +1,12 @@
 (function(){
   // init
-	document.getElementById("dashboard-items").innerHTML = '<div>' + "<a class="buddy buddy-no" href="blurbing.html">
-						<div class="avatar-no" style="background-image: url('img/profile-item.jpg') "></div>
-						<h1>Blurbing</h1><h2>Bio</h2>
-					</a>
-					<a class="buddy buddy-no" href="sprinting.html">
-						<div class="avatar-no" style="background-image: url('img/profile-item.jpg') "></div>
-						<h1>Sprinting</h1><h2>Show critical metadata for each plan</h2>
-					</a>
-					<a class="buddy buddy-no" href="meeting.html">
-						<div class="avatar-no" style="background-image: url('img/profile-item.jpg') "></div>
-						<h1>Meeting</h1><h2>7pm Chris @Koja</h2>
-					</a>
-					<a class="buddy buddy-no" href="reading.html">
-						<div class="avatar-no" style="background-image: url('img/profile-item.jpg') "></div>
-						<h1>Reading</h1><h2>Kintsugi</h2>
-					</a>
-					<a class="buddy buddy-no" href="listening.html">
-						<div class="avatar-no" style="background-image: url('img/profile-item.jpg') "></div>
-						<h1>Listening</h1><h2>Yelle - Tu Es Beau</h2>
-					</a>
-					<a class="buddy buddy-no" href="spending.html">
-						<div class="avatar-no" style="background-image: url('img/profile-item.jpg') "></div>
-						<h1>Spending</h1><h2>Cajun Crab</h2>
-					</a>
-					<a class="buddy buddy-no" href="exploring.html">
-						<div class="avatar-no" style="background-image: url('img/profile-item.jpg') "></div>
-						<h1>Exploring</h1><h2>Oslo</h2>
-					</a>
-					<hr />" + '</div>';
+	document.getElementById("dashboard-items").innerHTML = '<div>' +
+'<a class="buddy buddy-no" href="blurbing.html">' + '<div class="avatar-no" style="background-image: url('img/profile-item.jpg') ">' + '</div>' +
+'<h1>' + "Blurbing" + '</h1>' + '<h2>' + "Bio" + '</h2>' +
+'</a>' +
+'<a class="buddy buddy-no" href="sprinting.html">' +
+'<div class="avatar-no" style="background-image: url('img/profile-item.jpg') ">' + '</div>' +
+'<h1>' + "Sprinting" + '</h1>' + '<h2>' + "Show critical metadata for each plan" + '</h2>' +
+'</a>' +
+'<hr />' + '</div>';
 })();
