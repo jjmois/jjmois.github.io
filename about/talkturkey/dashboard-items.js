0245629d1d02346304aaa755c54c9afcbe5f8088
jjmois <heyjjmois@gmail.com>
2017-03-22 23:20:11 -0700

    Update dashboard-items.js

--- about/talkturkey/dashboard-items.js
+++ about/talkturkey/dashboard-items.js
@@ -1,12 +1,4 @@
 function(){
 // init
-document.getElementById("dashboard-items").innerHTML = '<a class="buddy buddy-no" href="blurbing.html">' +
-'<div class="avatar-no" style="background-image: url('img/profile-item.jpg') ">' + '</div>' +
-'<h1>' + "Blurbing" + '</h1>' + '<h2>' + "Bio" + '</h2>' +
-'</a>' +
-'<a class="buddy buddy-no" href="sprinting.html">' +
-'<div class="avatar-no" style="background-image: url('img/profile-item.jpg') ">' + '</div>' +
-'<h1>' + "Sprinting" + '</h1>' + '<h2>' + "Show critical metadata for each plan" + '</h2>' +
-'</a>' +
-'<hr />';
+document.getElementById("dashboard-items").innerHTML = 'abc';
 }
