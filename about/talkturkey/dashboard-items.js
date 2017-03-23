d704e1979f4d2cb509a962c52300cbc8e61ab28a
jjmois <heyjjmois@gmail.com>
2017-03-22 23:33:17 -0700

    Update dashboard-items.js

--- about/talkturkey/dashboard-items.js
+++ about/talkturkey/dashboard-items.js
@@ -1,12 +1,11 @@
 (function(){
   // init
-	document.getElementById("dashboard-items").innerHTML = '<a class="buddy buddy-no" href="blurbing.html">' +
+	document.getElementById("dashboard-items").innerHTML = '<div>' + '<a class="buddy buddy-no" href="blurbing.html">' +
 '<div class="avatar-no" style="background-image: url('img/profile-item.jpg') ">' + '</div>' +
 '<h1>' + "Blurbing" + '</h1>' + '<h2>' + "Bio" + '</h2>' +
 '</a>' +
 '<a class="buddy buddy-no" href="sprinting.html">' +
 '<div class="avatar-no" style="background-image: url('img/profile-item.jpg') ">' + '</div>' +
 '<h1>' + "Sprinting" + '</h1>' + '<h2>' + "Show critical metadata for each plan" + '</h2>' +
-'</a>' +
-'<hr />';
+'</a>' + '</div>';
 })();
